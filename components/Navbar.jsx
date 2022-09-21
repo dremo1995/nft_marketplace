625e5d04bf050e658a3fc3c5c8aef86dcc364d52
Andre <asm13august95@gmail.com>
2022-09-21 11:33:07 -0500

    fix assets folder and add navbar

---
+++ components/Navbar.jsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
+import Link from 'next/link';
+import images from '../assets';
+
+import { Button } from '.';
+
+const MenuItems = ({ isMobile, active, setActive }) => {
+  const generateLink = (index) => {
+    switch (index) {
+      case 0: return '/';
+      case 1: return 'created-nfts';
+      case 2: return 'my-nfts';
+      default: return '/';
+    }
+  };
+
+  return (
+    <ul className={`list-none flexCenter flex-row ${isMobile && 'flex-col h-full'}`}>
+      {['Explore NFTs', 'Listed NFTs', 'My NFTs'].map((item, index) => (
+        <li
+          key={index}
+          onClick={() => {
+            setActive(item);
+          }}
+          className={`flex flex-row items-center font-poppins font-semibold text-base dark:hover:text-white hover:text-nft-dark mx-3 ${active === item ? 'dark:text-white text-nft-black-1' : 'dark:text-nft-gray-3 text-nft-gray-2'}`}
+        >
+          <Link href={generateLink(index)}>{item}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const ButtonGroup = ({ setActive, router }) => {
+  const hasConnected = true;
+
+  return hasConnected ? (
+    <Button
+      btnName="Create"
+      classStyles="mx-2 rounded-xl"
+      handleCLick={() => {
+        setActive('');
+        router.push('/create-nft');
+      }}
+    />
+  ) : (<Button btnName="Connect" classStyles="mx-2 rounded-xl" />);
+};
+
+const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+  const { router } = useRouter();
+  const [active, setActive] = useState('Explore NFTs');
+
+  return (
+    <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
+      <div className="flex flex-1 flex-row justify-start" onClick={() => {}}>
+        <Link href="/">
+          <div className="flexCenter md:hidden cursor-pointer">
+            <Image src={images.logo02} objectFit="contain" width={75} height={75} alt="logo" />
+            <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-1">Ultraverse</p>
+          </div>
+
+        </Link>
+        <Link href="/">
+          <div className="hidden md:flex" onClick={() => {}}>
+            <Image src={images.logo02} objectFit="contain" width={100} height={100} />
+          </div>
+        </Link>
+      </div>
+      <div className="flex flex-initial flex-row justify-end">
+        <div className="flex items-center mr-2">
+          <input type="checkbox" className="checkbox" id="checkbox" onChange={() => setTheme(theme === 'dark' ? 'light' : 'dark')} />
+          <label htmlFor="checkbox" className="flexBetween w-8 h-4 bg-black rounded-2xl p-1 relative label">
+            <i className="fas fa-sun" />
+            <i className="fas fa-moon" />
+            <div className="w-3 h-3 absolute bg-white rounded-full ball" />
+          </label>
+        </div>
+      </div>
+
+      <div className="md:hidden flex">
+        <MenuItems active={active} setActive={setActive} />
+        <div className="ml-4">
+          <ButtonGroup setActive={setActive} router={router} />
+        </div>
+      </div>
+
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ pages/create-nft.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CreateNFT = () => (
+  <div className="text-3xl font-bold">CreateNFT</div>
+);
+
+export default CreateNFT;
